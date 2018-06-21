0efa069ddab2e80a657c30d47924d54e166aadea
Michael <myopia92@gmail.com>
2018-06-21 12:44:19 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/logo.png') {
+        myImage.setAttribute('src','images/logo2.png');
+    } else {
+        myImage.setAttribute('src','images/logo.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Hello ${myName}`;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = `Hello ${storedName}`;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
